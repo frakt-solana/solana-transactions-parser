703ee8d4a28e4db5d905a58f1a336248b34e0d70
sablevsky <vladislav1145@gmail.com>
2024-05-16 12:21:42 +0200

    Add readJson util in examples

--- examples/utils.ts
+++ examples/utils.ts
@@ -1,7 +1,13 @@
 import { promises as fs } from 'fs'
 
-type WriteJson = (params: { input: unknown; fileName?: string }) => Promise<void>
-
-export const writeJson: WriteJson = async ({ input, fileName = 'output.json' }) => {
+type WriteJsonParams = { input: unknown; fileName?: string }
+export async function writeJson({
+  input,
+  fileName = 'output.json',
+}: WriteJsonParams): Promise<void> {
   return void (await fs.writeFile(fileName, JSON.stringify(input, null, 2)))
+}
+
+export async function readJson(path: string): Promise<string> {
+  return await fs.readFile(path, { encoding: 'utf-8' })
 }
