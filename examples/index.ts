2cdd1d8c5be9dc5852efa2d683e666b3187a344d
sablevsky <vladislav1145@gmail.com>
2024-05-16 11:30:12 +0200

    Add deletedAccounts into example

--- examples/index.ts
+++ examples/index.ts
@@ -1,7 +1,7 @@
 import * as txnParser from '../src'
 import { IDL } from './idl'
 import { writeJson } from './utils'
-import { BorshCoder, web3 } from '@project-serum/anchor'
+import { web3 } from '@project-serum/anchor'
 import 'dotenv/config'
 import { map } from 'lodash'
 
@@ -10,11 +10,15 @@ const PROGRAM_PUBKEY = new web3.PublicKey('4tdmkuY6EStxbS6Y8s5ueznL3VPMSugrvQuDe
 const SIGNATURES = [
   '3rbpAv6ivURuFcnVeVPfSdC3eGNepwsu7cVyZLT9WA3uqhmhsnVpUinMGowAPimkYWXkQ3mP3qo7yTVi1fC2YxJc', //? Whitelist collection
   '5HvaakFQnymtm3223c49nN6PMA5E87pDooFyERgEJWE8WHJrXvPLMvFisGVRer7cQff6Zw54MxvDBLcLX4ewuYB6', //? Borrow staked banx
+  '4ojGspsKfVM8UogmZyCvVBCmVSpby8atE15nk4SncteyPKwCj4VHiP7hEimFM6joZEj3EgP6pGkSrPbbfMRLzKiT', //? Repayment call
+  '3dZfoHZQWT5bSc4Mc65WT8kzA1XojDZv9NWeTC617TToH6vxm8RoeJ1JqEaFWcmJtCietnMpwRop37bKLEMj4wYG', //? Subscribe banx adventure
+  '3FpB8EvCDcdaNbJR7gWpGwUuzF5Y1jLe7ne9pvLP1Xdmna7D37FqdRGk9jN56NhrAxjk1MwhVhLLkeLNWPhh8UCe', //? Refinance
+  '3ApeRKCx8p8Fffp18Uyxhqq77NKzKGpmxj19e2LpZy3kai8vb1Mc7SukGxsY2cWKwNvFaKQUkxUi4rWA4ve5ueMC', //? Borrow perpetual
 ]
 
-const TXN_SIGNATURE = SIGNATURES[1]
+const TXN_SIGNATURE = SIGNATURES[4]
 
-const coder = new BorshCoder(IDL)
+const coder = new txnParser.BorshCoder(IDL)
 
 const RPC_URL = process.env.RPC_URL ?? web3.clusterApiUrl('mainnet-beta')
 
@@ -47,7 +51,7 @@ export async function parsedTransactionExample() {
       transaction,
     })
 
-    const data = await txnParser.getAccountsData({
+    const { accountsData, deletedAccounts } = await txnParser.getAccountsData({
       accounts,
       coder,
       commitment,
@@ -56,7 +60,7 @@ export async function parsedTransactionExample() {
       programId: PROGRAM_PUBKEY,
     })
 
-    const convertedAccounts = map(data, ({ name, publicKey, data }) => {
+    const convertedAccounts = map(accountsData, ({ name, publicKey, data }) => {
       const convertedData = txnParser.convertValuesInAccount(data, {
         bnParser: (v) => {
           try {
@@ -76,7 +80,10 @@ export async function parsedTransactionExample() {
     })
 
     writeJson({
-      input: convertedAccounts,
+      input: {
+        accounts: convertedAccounts,
+        deletedAccounts: map(deletedAccounts, (a) => a.toBase58()),
+      },
       fileName: 'examples/output/parsedResult.json',
     })
   } catch (error) {
