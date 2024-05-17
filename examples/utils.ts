ac478ba6974e1279fc882657e1e3a49e5c85c0b2
sablevsky <vladislav1145@gmail.com>
2024-05-17 17:30:26 +0200

    Implement new utils for examples folder. wait, confirmTransactionByPollingSignatureStatus and abortOnTimeout

--- examples/utils.ts
+++ examples/utils.ts
@@ -1,3 +1,4 @@
+import { web3 } from '../src'
 import { promises as fs } from 'fs'
 
 type WriteJsonParams = { input: unknown; fileName?: string }
@@ -10,4 +11,55 @@ export async function writeJson({
 
 export async function readJson(path: string): Promise<string> {
   return await fs.readFile(path, { encoding: 'utf-8' })
+}
+
+export async function wait(time: number) {
+  return await new Promise((resolve) => setTimeout(resolve, time))
+}
+
+type ConfirmTransactionByPollingSignatureStatusParams = {
+  signature: string
+  connection: web3.Connection
+  commitment: web3.Commitment
+  /**
+   * Required because it is the only way to stop the loop if there were no errors
+   */
+  abortController: AbortController
+  /**
+   * Polling interval in seconds
+   */
+  refetchInterval: number
+}
+/**
+ * Can be used as fallback when websocket died (in connection.confirmTransaction) or RPC doen't support websockets at all
+ * Throws ConfirmTransactionError if something goes wrong
+ */
+export async function confirmTransactionByPollingSignatureStatus({
+  signature,
+  connection,
+  abortController,
+  commitment,
+  refetchInterval,
+}: ConfirmTransactionByPollingSignatureStatusParams): Promise<string | undefined> {
+  try {
+    while (!abortController.signal.aborted) {
+      const { value: signatureValue } = await connection.getSignatureStatus(signature, {
+        searchTransactionHistory: false,
+      })
+      if (signatureValue?.confirmationStatus === commitment) {
+        abortController.abort()
+        return signature
+      }
+      await wait(refetchInterval * 1000)
+    }
+  } catch (error) {
+    throw new Error('Error fetching transaction signature')
+  }
+}
+
+export async function abortOnTimeout(abortController: AbortController, timeout: number) {
+  await wait(timeout * 1000)
+  if (!abortController.signal.aborted) {
+    abortController.abort()
+  }
 }
